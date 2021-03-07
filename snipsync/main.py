87c9746f318fe648c02ee6f5561c300e7c09cbd1
sysid <sysid@gmx.de>
2021-03-07 11:41:29 +0100

    feat: configuration file handling

--- snipsync/main.py
+++ snipsync/main.py
@@ -1,3 +1,5 @@
+import configparser
+import json
 import logging
 import xml.etree.ElementTree as ET
 from pathlib import Path
@@ -5,23 +7,79 @@ from typing import List
 
 import typer
 
+from snipsync.settings import CONFIG_TEMPLATE
 from snipsync.ultisnip import UltiSnipsFileSource
 from snipsync.xml_snippet import XmlSnippet, read_ultisnips
 
 _log = logging.getLogger(__name__)
-app = typer.Typer()
+app = typer.Typer(help="Awesome snippet synchronization from Ultisnips to Intelij")
+
+APP_NAME = "snipsync"
+
+
+@app.callback()
+def setup(
+        ctx: typer.Context,
+):
+    """ Global setup """
+    app_dir = typer.get_app_dir(APP_NAME)
+    ctx.obj = dict(
+        app_dir=Path(app_dir),
+        config_path=Path(app_dir) / "config.ini",
+    )
+    typer.echo(f"About to execute command: {ctx.invoked_subcommand}")
 
 
 @app.command()
-def sync_to_intelij(
-    verbose: bool = typer.Option(False, "--verbose", "-v", help="verbose"),
-    save: bool = typer.Option(False, "--save", "-s", help="save it to InteliJ"),
-    context: List[str] = typer.Option(
-        ..., "--context", "-c", help="scope/context in InteliJ"
-    ),
-    ultisnip_file: Path = typer.Argument(..., help="ultisnips source", exists=True),
-    xmlsnip_file: Path = typer.Argument(..., help="xmlsnip target", exists=True),
+def dir(
+        ctx: typer.Context,
 ):
+    """
+    Show the location of the configuration file
+    """
+    app_dir = ctx.obj.get("app_dir")
+    config_path = ctx.obj.get("config_path")
+
+    if not config_path.is_file():
+        typer.echo(f"Config file {config_path} doesn't exist yet, creating...")
+        Path(app_dir).mkdir(parents=True, exist_ok=True)
+        with open(config_path, 'w') as textfile:
+            print(CONFIG_TEMPLATE, file=textfile)
+
+    typer.edit(CONFIG_TEMPLATE, filename=config_path)
+    typer.echo(f"Config file is: {config_path}")
+
+    config = configparser.ConfigParser(strict=True)
+    config.read(config_path)
+    ultisnips = Path(config['DEFAULT']['ultisnips']).resolve()
+    templates = Path(config['DEFAULT']['templates']).resolve()
+    ctx.obj['ultisnips'] = ultisnips
+    ctx.obj['templates'] = templates
+
+    ctx.obj['init'] = config['DEFAULT'].getboolean('init', fallback=False)
+    xxx = config.get('FILES', 'files')
+    try:
+        xxx = json.loads(xxx)
+    except json.JSONDecodeError as e:
+        typer.secho(f"Wrong JSON format in {config_path}: [FILES].", fg="red")
+        raise typer.Abort()
+    _ = None
+
+
+@app.command()
+def sync(
+        ctx: typer.Context,
+        verbose: bool = typer.Option(False, "--verbose", "-v", help="verbose"),
+        save: bool = typer.Option(False, "--save", "-s", help="save it to InteliJ"),
+        context: List[str] = typer.Option(
+            ..., "--context", "-c", help="scope/context in InteliJ"
+        ),
+        ultisnip_file: Path = typer.Argument(..., help="ultisnips source", exists=True),
+        xmlsnip_file: Path = typer.Argument(..., help="xmlsnip target", exists=True),
+):
+    """
+    Synchronizes Ultisnip snippets to InteliJ Live Templates
+    """
     typer.secho(f"-M- Syncing {ultisnip_file}", fg="green")
     xml_snippets = XmlSnippet(xmlsnip_file)
 
--- snipsync/settings.py
+++ snipsync/settings.py
@@ -21,3 +21,16 @@ ALLOWED_TOKENS = [
     VimLCodeToken,
     ShellCodeToken,
 ]
+
+CONFIG_TEMPLATE = """\
+[DEFAULT]
+templates = "$HOME/Library/Application Support/JetBrains/PyCharm2020.3/jba_config/templates"
+ultisnips = "$HOME/dev/binx/vim-config/UltiSnips"
+init = yes
+
+[FILES]
+files = [
+    sh.snippets
+    python.snippets
+]
+"""
